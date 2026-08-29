c48d8933f62bd4166a8221f9823ea7ad9954711a
kanwal-kamran <kanwalkamran335@gmail.com>
2026-08-29 13:59:08 +0500

    Changes in main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dashboard import open_dashboard
 from backup import automatic_daily_backup
+from database import init_database
 
 if __name__ == "__main__":
+    init_database()
     automatic_daily_backup()
     open_dashboard()
